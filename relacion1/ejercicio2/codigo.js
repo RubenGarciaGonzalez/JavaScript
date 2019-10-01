9ba184f5172d6c53f6d59db1c05624567815dbbe
RubenGarciaGonzalez <rubengg_00@hotmail.com>
2019-10-01 12:19:02 +0200

    SegundoEjercicio

---
+++ relacion1/ejercicio2/codigo.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo");
+console.log("Soy el primer script");
+let mensaje=alert("Hola Mundo!\nQué fácil es incluir \'comillas simples\' y \"comillas dobles\" ");
+let nombre=prompt("Dime tu nombre");
+let ventana=alert("Tu nombre es "+ nombre);
